3f3b731ca119f3e2dc1633081fc799babefbb2c6
Sion <mr.speedle@yahoo.cl>
2024-09-15 04:55:55 -0300

    3er commit, avances

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Selecciona todos los elementos con el atributo data-bs-toggle="tooltip"
+    var tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+  
+    // Inicializa cada tooltip
+    tooltips.forEach(function (tooltip) {
+      new bootstrap.Tooltip(tooltip, {
+        placement: "bottom" // Configura el tooltip para que aparezca debajo
+      });
+    });
+  });
