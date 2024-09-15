45d19c0514d7e6daabd9f56f4d93a8e28d14d752
Sion <mr.speedle@yahoo.cl>
2024-09-15 11:05:45 -0300

    5to commit, añadí bastante

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,14 +2,4 @@
 import "@hotwired/turbo-rails";
 import "controllers";
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Selecciona todos los elementos con el atributo data-bs-toggle="tooltip"
-    var tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-  
-    // Inicializa cada tooltip
-    tooltips.forEach(function (tooltip) {
-      new bootstrap.Tooltip(tooltip, {
-        placement: "bottom" // Configura el tooltip para que aparezca debajo
-      });
-    });
-  });
+
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,43 @@
-import { Application } from "@hotwired/stimulus"
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
 
 // Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
+application.debug = false;
+window.Stimulus = application;
 
-export { application }
+export { application };
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Selecciona todos los elementos con el atributo data-bs-toggle="tooltip"
+  var tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+
+  // Inicializa cada tooltip
+  tooltips.forEach(function (tooltip) {
+    new bootstrap.Tooltip(tooltip, {
+      placement: "bottom", // Configura el tooltip para que aparezca debajo
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var jsCarousel = document.getElementById("jsCarousel");
+  if (jsCarousel) {
+    new bootstrap.Carousel(jsCarousel);
+  }
+
+  var htmlCarousel = document.getElementById("htmlCarousel");
+  if (htmlCarousel) {
+    new bootstrap.Carousel(htmlCarousel);
+  }
+
+  var carouselGarlic = document.getElementById("carouselGarlic");
+  if (carouselGarlic) {
+    new bootstrap.Carousel(carouselGarlic);
+  }
+
+  var carouselVida = document.getElementById("carouselVida");
+  if (carouselVida) {
+    new bootstrap.Carousel(carouselVida);
+  }
+});
